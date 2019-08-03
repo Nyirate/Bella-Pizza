8e8b8edc4e6c83360f7f12ad380acd61a42ae3a8
Nyirate <rwakaemma34@gmail.com>
2019-08-03 18:48:32 +0200

    add codes for topping

--- index.js
+++ index.js
@@ -10,3 +10,77 @@ function Pizorder(size, toppings, crust, number, delivery) {
 Pizorder.prototype.information = function() {
   return this.size + ", " + this.toppings + ", " + this.crust + ", " + this.number + ", " + this.delivery
 }
+
+$(document).ready(function(){
+  $('button').click(function(){
+    // pizza size
+    if($('#small:checked').val()){
+       var prix= '500Ksh';
+      var size=$('#small').val(); + " " + "prix=500Ksh"
+      console.log(size)
+    }
+    if($('#medium:checked').val()){
+      var prix= '600Ksh';
+     var size=$('#medium').val(); + " " + "prix=600Ksh"
+     console.log(size)
+   }
+   if($('#large:checked').val()){
+    var prix= '800Ksh';
+   var size=$('#large').val(); + " " + "prix=800Ksh"
+   console.log(size)
+   }
+
+  //  pizza toppings
+  if($('#pep:checked').val()){
+    var price= '400Ksh';
+   var topping=$('#pep').val(); + " " + "prix=400Ksh"
+   console.log(topping)
+  }
+  if($('#oni:checked').val()){
+    var price= '200Ksh';
+   var topping=$('#oni').val(); + " " + "prix=200Ksh"
+   console.log(topping)
+  }
+  if($('#saus:checked').val()){
+    var price= '600Ksh';
+   var topping=$('#saus').val(); + " " + "prix=600Ksh"
+   console.log(topping)
+  }
+  if($('#bac:checked').val()){
+    var price= '450Ksh';
+   var topping=$('#bac').val(); + " " + "prix=450Ksh"
+   console.log(topping)
+  }
+  if($('#ches:checked').val()){
+    var price= '500Ksh';
+   var topping=$('#ches').val(); + " " + "prix=500Ksh"
+   console.log(topping)
+  }
+  if($('#pine:checked').val()){
+    var price= '500Ksh';
+   var topping=$('#pine').val(); + " " + "prix=500Ksh"
+   console.log(topping)
+  }
+  if($('#spine:checked').val()){
+    var price= '300Ksh';
+   var topping=$('#spine').val(); + " " + "prix=300Ksh"
+   console.log(topping)
+  }
+  if($('#cris:checked').val()){
+    var prices= '1000Ksh';
+   var crust=$('#cris').val(); + " " + "prix=1000Ksh"
+   console.log(crust)
+  }
+  if($('#stuf:checked').val()){
+    var prices= '1200Ksh';
+   var crust=$('#stuf').val(); + " " + "prix=1200Ksh"
+   console.log(crust)
+  }
+  if($('#glut:checked').val()){
+    var prices= '1500Ksh';
+   var crust=$('#glut').val(); + " " + "prix=1500Ksh"
+   console.log(crust)
+  }
+  });
+});
+
